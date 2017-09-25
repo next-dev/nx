25a5b8ff95eb832d6590f198552923dc679616f4
Unknown <mattie.davies@gmail.com>
2017-09-24 22:34:57 -0400

    LDDR, CPDR, INDR, OTDR passed test.  Opcodes up to $f8 pass.

--- src/z80.h
+++ src/z80.h
@@ -1468,6 +1468,93 @@ void z80StepED(Z80* Z, i64* tState)
             }
             break;
 
+        case 0xb8:  // LDDR
+            v = PEEK(HL);
+            POKE(DE, v);
+            CONTEND(DE, 1, 2);
+            --BC;
+            v += A;
+            F = (F & (F_CARRY | F_ZERO | F_SIGN)) | (BC ? F_PARITY : 0) |
+                (v & F_3) | ((v & 0x02) ? F_5 : 0);
+            if (BC)
+            {
+                CONTEND(DE, 1, 5);
+                PC -= 2;
+                MP = PC + 1;
+            }
+            --DE;
+            --HL;
+            break;
+
+        case 0xb9:  // CPDR
+            {
+                v = PEEK(HL);
+                u8 t = A - v;
+                u8 lookup = ((A & 0x08) >> 3) | ((v & 0x08) >> 2) | ((t & 0x08) >> 1);
+                CONTEND(HL, 1, 5);
+                --BC;
+                F = (F & F_CARRY) | (BC ? (F_PARITY | F_NEG) : F_NEG) |
+                    kHalfCarrySub[lookup] | (t ? 0 : F_ZERO) | (t & F_SIGN);
+                if (F & F_HALF) --t;
+                F |= (t & F_3) | ((t & 0x02) ? F_5 : 0);
+                if ((F & (F_PARITY | F_ZERO)) == F_PARITY)
+                {
+                    CONTEND(HL, 1, 5);
+                    PC -= 2;
+                    MP = PC + 1;
+                }
+                else
+                {
+                    --MP;
+                }
+                --HL;
+            }
+            break;
+
+        case 0xba:  // INDR
+            {
+                u8 t1, t2;
+                CONTEND(IR, 1, 1);
+                t1 = ioIn(Z->io, BC, tState);
+                POKE(HL, t1);
+                MP = BC - 1;
+                --B;
+                t2 = t1 + C - 1;
+                F = (t1 & 0x80 ? F_NEG : 0) |
+                    ((t2 < t1) ? F_HALF | F_CARRY : 0) |
+                    (gParity[(t2 & 0x07) ^ B] ? F_PARITY : 0) |
+                    gSZ53[B];
+                if (B)
+                {
+                    CONTEND(HL, 1, 5);
+                    PC -= 2;
+                }
+                --HL;
+            }
+            break;
+
+        case 0xbb:  // OTDR
+            {
+                u8 t1, t2;
+                CONTEND(IR, 1, 1);
+                t1 = PEEK(HL);
+                --B;
+                MP = BC - 1;
+                ioOut(Z->io, BC, t1, tState);
+                --HL;
+                t2 = t1 + L;
+                F = (t1 & 0x80 ? F_NEG : 0) |
+                    ((t2 < t1) ? F_HALF | F_CARRY : 0) |
+                    (gParity[(t2 & 0x07) ^ B] ? F_PARITY : 0) |
+                    gSZ53[B];
+                if (B)
+                {
+                    CONTEND(BC, 1, 5);
+                    PC -= 2;
+                }
+            }
+            break;
+
         default:
             goto invalid_instruction;
         }
