b4a5314cf1d59e945c2e52440f808e9defacd7f3
Unknown <mattie.davies@gmail.com>
2017-10-02 18:56:58 -0400

    Added Windows console.

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,9 @@
+//----------------------------------------------------------------------------------------------------------------------
+// NX - Next Emulator
+//----------------------------------------------------------------------------------------------------------------------
+
+#define NX_DEBUG_CONSOLE    1
+
 #include <SFML/Graphics.hpp>
 
 #include <cstdint>
@@ -141,6 +147,8 @@ void key(Nx& N, sf::Keyboard::Key key, bool down)
     Key key1 = Key::COUNT;
     Key key2 = Key::COUNT;
 
+    printf("KEY [%s]: %d\n", down ? "DOWN" : " UP ", key);
+
     using K = sf::Keyboard;
 
     switch (key)
@@ -211,11 +219,52 @@ void key(Nx& N, sf::Keyboard::Key key, bool down)
 }
 
 //----------------------------------------------------------------------------------------------------------------------
+// Console
+//----------------------------------------------------------------------------------------------------------------------
+
+#ifdef _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <conio.h>
+#include <fcntl.h>
+#include <io.h>
+
+void console()
+{
+    AllocConsole();
+    SetConsoleTitleA("Debug Window");
+
+    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    int hCrt = _open_osfhandle((intptr_t)handle_out, _O_TEXT);
+    FILE* hf_out = _fdopen(hCrt, "w");
+    setvbuf(hf_out, NULL, _IONBF, 1);
+    freopen("CONOUT$", "w", stdout);
+
+    HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
+    hCrt = _open_osfhandle((intptr_t)handle_in, _O_TEXT);
+    FILE* hf_in = _fdopen(hCrt, "r");
+    setvbuf(hf_in, NULL, _IONBF, 0);
+    freopen("CONIN$", "r", stdin);
+
+    DWORD mode;
+    GetConsoleMode(handle_out, &mode);
+    mode |= 0x4;
+    SetConsoleMode(handle_out, mode);
+}
+
+#endif
+
+//----------------------------------------------------------------------------------------------------------------------
 // Main loop
 //----------------------------------------------------------------------------------------------------------------------
 
 int main()
 {
+#if NX_DEBUG_CONSOLE
+    console();
+#endif
+
     Host host;
     sf::RenderWindow window(sf::VideoMode(kWindowWidth * 4, kWindowHeight * 4), "NX " NX_VERSION);
     sf::Texture tex;
