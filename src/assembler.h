a5f1c845eb077b947d696b7c152f0916dd74dad6
Unknown <mattie.davies@gmail.com>
2017-11-01 16:22:52 -0400

    AssemblerOverlay interface added.

--- src/assembler.h
+++ src/assembler.h
@@ -1,1 +1,27 @@
 //----------------------------------------------------------------------------------------------------------------------
+// Assembler Overlay
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "ui.h"
+#include "editor.h"
+
+#include <vector>
+
+class Assembler final : public Overlay
+{
+public:
+    Assembler(Nx& nx);
+
+    void render(Draw& draw) override;
+    void key(sf::Keyboard::Key key, bool down, bool shift, bool ctrl, bool alt) override;
+    void text(char ch) override;
+    void const vector<string>& commands() const override;
+
+private:
+    EditorWindow    m_window;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
