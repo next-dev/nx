99056e2e23213f6032738b9f6d14e6ec9b174c23
Matt Davies <mattie.davies@gmail.com>
2018-05-06 16:07:55 -0400

    Added build.bat

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set "folder=%cd%"
+
+call gen.bat
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+cd /d %folder%
+pushd _build
+msbuild nx.sln /t:rebuild /p:configuration=Release /p:platform=Win64
+popd
