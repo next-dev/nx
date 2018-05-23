ac6d0402852ba42b9eee3d3a9c000110bd40591c
Matt Davies <mattie.davies@gmail.com>
2018-05-23 09:48:21 -0400

    Fixed build.bat to support VS Professional.

--- build.bat
+++ build.bat
@@ -3,7 +3,14 @@
 set "folder=%cd%"
 
 call gen.bat
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+) else (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+    )
+)
 cd /d %folder%
 pushd _build
 msbuild nx.sln /t:rebuild /p:configuration=Release /p:platform=Win64
